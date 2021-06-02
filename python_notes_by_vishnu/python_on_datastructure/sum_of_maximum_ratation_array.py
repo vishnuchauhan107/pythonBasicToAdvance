58b3e888bd65f34464129299b410d1c7c053a67c
vishnu chauhan <vishnu.chauhan@gla.ac.in>
2021-06-02 21:51:00 +0530

    modify sum_of_maximum_ratation_array.py

--- python_notes_by_vishnu/python_on_datastructure/sum_of_maximum_ratation_array.py
+++ python_notes_by_vishnu/python_on_datastructure/sum_of_maximum_ratation_array.py
@@ -2,11 +2,9 @@ def maxSum(arr):
     arrSum = 0
     currVal = 0
     n = len(arr)
-
     for i in range(0, n):
-        arrSum = arrSum + arr[i]
-        currVal = currVal + (i * arr[i])
-
+       arrSum = arrSum + arr[i]
+       currVal = currVal + (i * arr[i])
     maxVal = currVal
     for j in range(1, n):
         currVal = currVal + arrSum - n * arr[n - j]
